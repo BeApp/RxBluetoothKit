90d473b59d433126174ed0aab429e45de0186ffd
jberkhei <jberkhei@users.noreply.github.com>
2023-09-29 13:20:33 -0400

    Update Unimplemented.swift

--- Source/Unimplemented.swift
+++ Source/Unimplemented.swift
@@ -5,9 +5,12 @@ func unimplementedFunction(file: String = #file, function: String = #function, l
 }
 
 extension Observable {
+extension RxSwift.Observable {
     static func unimplemented(file: String = #file, function: String = #function, line: Int = #line)
         -> Observable<Element> {
+    -> RxSwift.Observable<Element> {
         unimplementedFunction(file: file, function: function, line: line)
         return Observable<Element>.empty()
+            return RxSwift.Observable<Element>.empty()
     }
 }
